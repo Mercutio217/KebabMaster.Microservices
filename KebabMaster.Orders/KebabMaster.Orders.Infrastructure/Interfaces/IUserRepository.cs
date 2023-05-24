f563a124134033aaf9ef2555572c3337f143dcad
Lucas <lukasz.bielenin@microsoft.wsei.edu.pl>
2023-05-24 19:48:07 +0200

    Add logger

---
+++ KebabMaster.Orders/KebabMaster.Orders.Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using KebabMaster.Orders.Domain.DTOs.Authorization;
+using KebabMaster.Orders.Infrastructure.DTOs.Authorization;
+
+namespace KebabMaster.Orders.Infrastructure.Interfaces;
+
+public interface IUserRepository
+{
+    Task CreateUser(User user);
+    Task<User?> GetUserByEmail(string email);
+    Task<User?> GetUserByName(string name);
+    
+    Task<Role?> GetRoleByName(string name);
+}
---
+++ KebabMaster.Orders/KebabMaster.Orders.Infrastructure/Logger/ApplicationLogger.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using KebabMaster.Orders.Domain.Entities;
+using KebabMaster.Orders.Domain.Exceptions;
+using KebabMaster.Orders.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace KebabMaster.Orders.Infrastructure.Logger;
+
+public class ApplicationLogger : IApplicationLogger
+{
+    private readonly ILogger<Order> _logger;
+    public ApplicationLogger(ILogger<Order> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogGetStart(object request)
+    {
+        _logger.LogInformation($"Start getting Orders by filter {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogGetEnd(object request)
+    {
+        _logger.LogInformation($"Finish getting Orders by filter {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogPostStart(object request)
+    {
+        _logger.LogInformation($"Start creating Orders with request {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogPostEnd(object request)
+    {
+        _logger.LogInformation($"Finish creating Orders with request {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogDeleteStart(object request)
+    {
+        _logger.LogInformation($"Start deleting Orders with request {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogDeleteEnd(object request)
+    {
+        _logger.LogInformation($"Finish deleting Orders with request {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogPutEnd(object request)
+    {
+        _logger.LogInformation($"Start updating Orders with request {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogPutStart(object request)
+    {
+        _logger.LogInformation($"Finish updating Orders with request {JsonSerializer.Serialize(request)}");
+    }
+
+    public void LogException(Exception exception)
+    {
+        _logger.LogError($"Error occured during processing entity, Exception: {exception}");
+    }
+    
+    public void LogValidationException(ApplicationValidationException validationException)
+    {
+        _logger.LogError($"There was error validation exception: {validationException.GetValidationErrorMessage()}");
+    }
+}
