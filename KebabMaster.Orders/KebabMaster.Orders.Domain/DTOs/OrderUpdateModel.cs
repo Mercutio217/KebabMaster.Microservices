67d959c540794f32b0ebd92178aac82eab6d8c1f
Lucas <lukasz.bielenin@microsoft.wsei.edu.pl>
2023-05-24 19:47:42 +0200

    Add domain dtos

---
+++ KebabMaster.Orders/KebabMaster.Orders.Domain/DTOs/OrderUpdateModel.cs
@@ -0,0 +1,26 @@
+﻿using KebabMaster.Orders.Domain.Entities;
+using KebabMaster.Orders.Domain.Exceptions;
+
+namespace KebabMaster.Orders.Domain.DTOs;
+
+public class OrderUpdateModel
+{
+    public Guid Id { get; private set; }
+    public Address Address { get; private set; }
+    public IEnumerable<OrderItem> OrderItems { get; private set; }
+
+    public OrderUpdateModel(Guid id, Address address, IEnumerable<OrderItem> orderItems)
+    {
+        Id = id;
+        Address = address;
+        OrderItems = orderItems;
+    }
+
+    public static OrderUpdateModel Create(Guid? guid, Address address, IEnumerable<OrderItem> items)
+    {
+        if (!guid.HasValue)
+            throw new MissingMandatoryPropertyException<OrderUpdateModel>(nameof(Id));
+
+        return new(guid.Value, address, items);
+    }
+}
