882a19e6906a11729c6a041a733fa4b36f6264a7
Lucas <lukasz.bielenin@microsoft.wsei.edu.pl>
2023-05-24 22:15:29 +0200

    Add authorization controllers

---
+++ KebabMaster.Authorization/KebabMaster.Authorization.WebApi/Controllers/ApplicationBaseController.cs
@@ -0,0 +1,49 @@
+﻿using KebabMaster.Authorization.Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KebabMaster.Authorization.Controllers;
+
+public abstract class ApplicationBaseController  : ControllerBase
+{
+    protected async Task<IActionResult> Execute(Func<Task> function, ActionResult actionResult)
+    {
+        try
+        {
+            await function();
+            
+            return actionResult;
+        }
+        catch (NotFoundException)
+        {
+            return StatusCode(StatusCodes.Status404NotFound);
+        }
+        catch (ApplicationValidationException validationException)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, validationException.GetValidationErrorMessage());
+        }
+        catch (Exception exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    protected async Task<ActionResult<T>> Execute<T>(Func<Task<T>> function) where T : new()
+    {
+        try
+        {
+            return await function();
+        }
+        catch (NotFoundException)
+        {
+            return StatusCode(StatusCodes.Status404NotFound);
+        }
+        catch (ApplicationValidationException validationException)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, validationException.GetValidationErrorMessage());
+        }
+        catch (Exception exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
