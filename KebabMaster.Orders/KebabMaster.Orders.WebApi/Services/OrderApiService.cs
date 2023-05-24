5c525b8cd4d9e62caebe442ed56e5e86f792ef1e
Lucas <lukasz.bielenin@microsoft.wsei.edu.pl>
2023-05-24 19:48:50 +0200

    Add services

---
+++ KebabMaster.Orders/KebabMaster.Orders.WebApi/Services/OrderApiService.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using KebabMaster.Orders.Domain;
+using KebabMaster.Orders.Domain.DTOs;
+using KebabMaster.Orders.Domain.Entities;
+using KebabMaster.Orders.Domain.Entities.Base;
+using KebabMaster.Orders.Domain.Exceptions;
+using KebabMaster.Orders.Domain.Interfaces;
+using KebabMaster.Orders.DTOs;
+
+namespace KebabMaster.Orders.Services;
+
+public class OrderApiService : IOrderApiService
+{
+    private readonly IOrderService _orderService;
+    private readonly IMapper _mapper;
+    private readonly IApplicationLogger _logger;
+
+    public OrderApiService(
+        IOrderService orderService,
+        IMapper mapper,
+        IApplicationLogger logger)
+    {
+        _orderService = orderService;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task CreateOrder(OrderRequest order)
+    {
+        await Execute(async () =>
+        {
+            _logger.LogPostStart(order);
+            await _orderService.CreateOrder(_mapper.Map<Order>(order));
+
+            _logger.LogPostEnd(order);
+        });
+    }
+
+    public async Task<ApplicationResponse<OrderResponse>> GetOrdersAsync(OrderFilter filter) =>
+        await Execute(async () =>
+        {
+            _logger.LogGetStart(filter);
+            IEnumerable<Order> result = await _orderService.GetOrdersAsync(filter);
+            _logger.LogGetEnd(filter);
+            return new ApplicationResponse<OrderResponse>(
+                _mapper.Map<IEnumerable<OrderResponse>>(result));
+        });
+
+    public async Task<OrderResponse> GetOrderById(Guid id) =>
+        await Execute(async () =>
+        {
+            _logger.LogGetStart(id);
+            Order result = await Execute<Order>(() => _orderService.GetOrderByIdAsync(id));
+            _logger.LogGetEnd(id);
+
+            return _mapper.Map<OrderResponse>(result);
+        });
+
+    public async Task DeleteOrder(Guid id) =>
+        await Execute(async () =>
+        {
+            _logger.LogDeleteStart(id);
+            await _orderService.DeleteOrder(id);
+            _logger.LogDeleteEnd(id);
+        });
+
+    public async Task UpdateOrder(OrderUpdateRequest order) =>
+        await Execute(async () =>
+        {
+            _logger.LogDeleteStart(order);
+            var dto = _mapper.Map<OrderUpdateModel>(order);
+
+            await _orderService.UpdateOrder(dto);
+
+            _logger.LogDeleteEnd(order);
+        });
+
+    private async Task Execute(Func<Task> function)
+    {
+        try
+        {
+            await function();
+        }
+        catch (ApplicationValidationException validationException)
+        {
+            _logger.LogValidationException(validationException);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogException(exception);
+        }
+    }
+
+    private async Task<T> Execute<T>(Func<Task<T>> function)
+    {
+        try
+        {
+            return await function();
+        }
+        catch (ApplicationValidationException validationException)
+        {
+            _logger.LogValidationException(validationException);
+            throw;
+        }
+        catch (Exception exception)
+        {
+            _logger.LogException(exception);
+            throw;
+        }
+    }
+}
---
+++ KebabMaster.Orders/KebabMaster.Orders.WebApi/Services/UserManagementService.cs
@@ -0,0 +1,105 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using KebabMaster.Orders.Domain.DTOs.Authorization;
+using KebabMaster.Orders.Domain.Exceptions;
+using KebabMaster.Orders.DTOs;
+using KebabMaster.Orders.Infrastructure.DTOs.Authorization;
+using KebabMaster.Orders.Infrastructure.Interfaces;
+using KebabMaster.Orders.Interfaces;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+
+namespace KebabMaster.Orders.Services;
+
+public class UserManagementService : IUserManagementService
+{
+    private readonly IConfiguration _configuration;
+    private readonly IUserRepository _repository;
+    
+    public UserManagementService(
+        IConfiguration configuration,
+        IUserRepository repository)
+    {
+        _configuration = configuration;
+        _repository = repository;
+    }
+
+    public async Task CreateUser(RegisterModel model)
+    {
+        User user = User.Create(model.Email, model.UserName, model.Name, model.Surname);
+
+        var hash = HashString(model.Password);
+
+        user.PaswordHash = hash;
+
+        var role = await _repository.GetRoleByName("Admin");
+        user.Roles = new List<Role>() { role };
+
+        await _repository.CreateUser(user);
+    }
+
+
+    public async Task<TokenResponse> Login(LoginModel model)
+    {
+        var user = await _repository.GetUserByEmail(model.Email);
+        if (user is null)
+            throw new UnauthorizedException(model.Email);
+
+        string hashString = HashString(model.Password);
+        
+        if (hashString != user.PaswordHash)
+            throw new Exception();
+
+        var claims = new List<Claim>()
+        {
+            new (ClaimTypes.Name, user.UserName),
+            new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+        foreach (var role in user.Roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role,role.Name));
+        }
+        
+        var token = GetToken(claims);
+
+        return new TokenResponse()
+        {
+            ExpiresAt = token.ValidTo,
+            Token = new JwtSecurityTokenHandler().WriteToken(token),
+        };
+    }
+    
+    private JwtSecurityToken GetToken(List<Claim> authClaims)
+    {
+        SymmetricSecurityKey authSigningKey = 
+            new (Encoding.UTF8.GetBytes(_configuration["TokenData:Secret"]));
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["TokenData:ValidIssuer"],
+            audience: _configuration["TokenData:ValidAudience"],
+            expires: DateTime.Now.AddHours(3),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return token;
+    }
+    private  string HashString(string text)
+    {
+        if (String.IsNullOrEmpty(text))
+            throw new MissingMandatoryPropertyException<User>("Password");
+
+        using var sha = new System.Security.Cryptography.SHA256Managed();
+        byte[] textBytes = Encoding.UTF8.GetBytes(text);
+        byte[] hashBytes = sha.ComputeHash(textBytes);
+        
+        // Convert back to a string, removing the '-' that BitConverter adds
+        string hash = BitConverter
+            .ToString(hashBytes)
+            .Replace("-", String.Empty);
+
+        return hash;
+    }
+}
