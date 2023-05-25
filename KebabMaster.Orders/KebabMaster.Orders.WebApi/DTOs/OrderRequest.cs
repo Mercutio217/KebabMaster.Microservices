eecde9d33e1487d0d264ccbba1f974aa7898ea4e
Lucas <lukasz.bielenin@microsoft.wsei.edu.pl>
2023-05-25 18:49:47 +0200

    Fix comms

--- KebabMaster.Orders/KebabMaster.Orders.WebApi/DTOs/OrderRequest.cs
+++ KebabMaster.Orders/KebabMaster.Orders.WebApi/DTOs/OrderRequest.cs
@@ -4,7 +4,7 @@ namespace KebabMaster.Orders.DTOs;
 
 public class OrderRequest
 {
-    public string Email { get; set; }
-    public AddressDto Address { get; set; }
-    public IEnumerable<OrderItemDto> OrderItems { get; set; }
+    public string? Email { get; set; }
+    public AddressDto? Address { get; set; }
+    public IEnumerable<OrderItemDto>? OrderItems { get; set; }
 }
